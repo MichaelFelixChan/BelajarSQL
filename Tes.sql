03b063366a805bcb23ccb0fa9fbba16a3b1b3eb5
Bryan <michaelfelixchandra_2327240030@mhs.mdp.ac.id>
2025-02-27 13:04:25 +0700

    Update Tes.sql
    Update coding SQL ERD pada ppt yang dibahas hari ini

--- Tes.sql
+++ Tes.sql
@@ -1,19 +1,41 @@
--- membuat database
+-- tabel anggota (id_anggota, nama, alamat, no_telp)
+-- tabel pustakawan (id_pustakawan, nama, alamat, no_telepon)
+-- tabel penulis (id_penulis, nama, alamat, bidang_ilmu)
+-- tabel buku (id_buku, judul, tahun_terbit, id_penerbit)
+-- tabel penulisan (id_penulis, id_buku)
+-- tabel peminjaman (id_peminjaman, tanggal_pinjam, lama_pinjam, tanggal_kembali, id_anggota, id_pustakawan)
+
+-- Membuat database
 CREATE DATABASE Perpustakaan;
 
--- mengaktifkan database 
+-- Mengaktifkan database 
 USE Perpustakaan;
 
--- membuat tabel
+-- Membuat tabel
 CREATE TABLE Anggota(
-	ID_Anggota char(5) NOT NULL PRIMARY KEY,
-    Nama varchar(50) NOT NULL,
+	ID_Anggota char(5) NOT NULL PRIMARY KEY, -- Harusnya panjang data yaitu 10
+    Nama varchar(50) NOT NULL, -- Harusnya panjang data nama yaitu 100
     Alamat varchar(200) NOT NULL,
-    No_Telp varchar(12) NOT NULL
+    No_Telp varchar(12) NOT NULL -- Harusnya panjang data yaitu 25
 );
 
 -- Menampilkan isi tabel anggota
 desc anggota;
+
+CREATE TABLE Pustakawan(
+	ID_Pustakawan char(10) NOT NULL PRIMARY KEY,
+    Nama varchar(100) NOT NULL,
+    Alamat varchar(200) NOT NULL,
+    No_Telepon varchar(25) NOT NULL
+);
+
+CREATE TABLE Penulis(
+	ID_Penulis char(5) NOT NULL PRIMARY KEY,
+    Nama varchar(50) NOT NULL,
+    Alamat varchar(200) NOT NULL,
+    Bidang_Ilmu varchar(50) NOT NULL
+);
+
 
 -- CREATE TABLE Peminjaman(
 -- 	ID_Peminjaman char(5) NOT NULL PRIMARY KEY,
@@ -24,13 +46,6 @@ desc anggota;
 --     TanggalKembali date NOT NULL,
 --     FOREIGN KEY (ID_Anggota) REFERENCES Anggota(ID_Anggota),
 --     FOREIGN KEY (ID_Pustakawan) REFERENCES Pustakawan(ID_Pustakawan)
--- );
-
--- CREATE TABLE Pustakawan(
--- 	ID_Pustakawan char(5) NOT NULL PRIMARY KEY,
---     Nama varchar(50) NOT NULL,
---     Alamat varchar(200) NOT NULL,
---     No_Telepon varchar(12) NOT NULL
 -- );
 
 -- CREATE TABLE Buku(
@@ -48,21 +63,14 @@ desc anggota;
 --     No_Telepon varchar(12) NOT NULL
 -- );
 
-CREATE TABLE Penulis(
-	ID_Penulis char(5) NOT NULL PRIMARY KEY,
-    Nama varchar(50) NOT NULL,
-    Alamat varchar(200) NOT NULL,
-    Bidang_Ilmu varchar(50) NOT NULL
-);
-
--- CREATE TABLE Penulis_Buku(
+-- CREATE TABLE Penulisan(
 -- 	ID_Penulis char(5) NOT NULL,
 --     ID_Buku char(5) NOT NULL,
 --     FOREIGN KEY (ID_Penulis) REFERENCES Penulis(ID_Penulis),
 --     FOREIGN KEY (ID_Buku) REFERENCES Buku(ID_Buku)
 -- );
 
--- CREATE TABLE Peminjaman_Buku(
+-- CREATE TABLE Detail_Peminjaman(
 -- 	ID_Peminjaman char(5) NOT NULL,
 --     ID_Buku char(5) NOT NULL,
 --     FOREIGN KEY (ID_Peminjaman) REFERENCES Peminjaman(ID_Peminjaman),
